2765a0a8dd0931c895fe5e702d390e3a54f2b490
Akash Sharma <lucky1612000@gmail.com>
2021-06-27 21:23:55 +0530

    added support for cors

--- src/main/java/com/akash/controller/ReadingController.java
+++ src/main/java/com/akash/controller/ReadingController.java
@@ -3,6 +3,7 @@ package com.akash.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,6 +12,7 @@ import com.akash.entity.Reading;
 import com.akash.service.ReadingService;
 
 @RestController
+@CrossOrigin(originPatterns = "http://*")
 public class ReadingController {
 	
 	@Autowired 
